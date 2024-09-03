4d0ef14ad338b9504dc7c6bba338905e4418a411
sharma <sharma@DESKTOP-IS2799H>
2024-09-03 12:00:28 +0530

    Adding more code to JavaScript Basics Practice

--- Basics1.js
+++ Basics1.js
@@ -4,3 +4,17 @@ let b = 20;
 let c = a + b;
 console.log(c)
 console.log(typeof (a)) //typeof(c)
+
+let Name = "Sachin";
+console.log(typeof (Name)) //typeof(Name)
+
+let required = true;
+console.log(typeof (required));
+
+required = false;
+console.log(typeof (required));
+
+//We cannot redeclare a variable using let keyword but with var keyword we can do it.
+
+// It will reverse the boolean value Example: False will become True and True will become False
+console.log(!required);
