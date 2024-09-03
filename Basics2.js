79530b80055f7ad1b08176d86aee7a50df8c012d
sharma <sharma@DESKTOP-IS2799H>
2024-09-03 12:09:32 +0530

    Code Formatted

--- Basics2.js
+++ Basics2.js
@@ -1,31 +1,31 @@
 const flag = true;
 
-if (!flag){
+if (!flag) {
     console.log("Condition is fullfilled");
 }
-else{
+else {
     console.log("Condition is not fullfilled");
 }
 
 let i = 0;
-while(i < 10){
+while (i < 10) {
     console.log(i);
     i++;
 }
-do{
-i++;
+do {
+    i++;
 }
-while(i<10);{
-console.log(i);
+while (i < 10); {
+    console.log(i);
 }
 
-for(let k=1;k<10;k++){
-    if(k%2==0 && k%5==0){
+for (let k = 1; k < 10; k++) {
+    if (k % 2 == 0 && k % 5 == 0) {
         console.log(k);
     }
 }
 let required = true;
-while(required);{
+while (required); {
     console.log(required);
     required = false;
 }
